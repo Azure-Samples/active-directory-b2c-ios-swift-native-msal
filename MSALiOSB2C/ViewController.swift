3659eab59c1d7787887522eaeba390736ea17cad
Brandon Werner <brandwe@microsoft.com>
2017-05-04 19:01:49 -0700

    Cleanup

--- MSALiOSB2C/ViewController.swift
+++ MSALiOSB2C/ViewController.swift
@@ -1,16 +1,32 @@
+//------------------------------------------------------------------------------
 //
-//  ViewController.swift
-//  MSALiOSB2C
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
 //
-//  Created by Brandon Werner on 5/1/17.
-//  Copyright © 2017 Brandon Werner. All rights reserved.
+// This code is licensed under the MIT License.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
 
 import UIKit
 import MSAL
-
-
-
 
 class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate  {
     
@@ -108,8 +124,6 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate 
 
     
     @IBAction func callApi(_ sender: UIButton) {
-        
-        do {
             
             
             let sessionConfig = URLSessionConfiguration.default
@@ -120,29 +134,17 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate 
             
             urlSession.dataTask(with: request) { data, response, error in
                 
-                if (error == nil) {
-                    
+            
                     let result = try? JSONSerialization.jsonObject(with: data!, options: [])
                     DispatchQueue.main.async {
                         if result != nil {
-                            
-                            self.loggingText.text = result.debugDescription
-                            
-                            
+                            self.loggingText.text = "API response: \(result.debugDescription)"
                         }
                         else {
                             self.loggingText.text = "Nothing returned from API"
                         }
-                    } }
-                else {
-                    self.loggingText.text = "Got error: \(error?.localizedDescription ?? "No description")"
-                }
+                    }
                 }.resume()
-            
-        }
-        catch {
-            self.loggingText.text = "Unable to call API: \(error)"
-        }
         
     }
     
